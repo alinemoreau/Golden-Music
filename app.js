69d5566a703c5f54ffd804a3f7e018a80a69e885
Alex Garcia <alexgarcia@Alexs-MacBook-Pro.local>
2022-07-31 23:30:18 -0400

    added icon and event listener

--- app.js
+++ app.js
@@ -18,3 +18,9 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+//user icon
+let icon = document.querySelector('#icon');
+icon.addEventListener('click',function() {
+  alert("You clicked me!");
+})
